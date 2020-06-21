59f9c5291f066a172245f7798fe57f376f4ed2d2
Martin Kadlec <kadlec.martin@ifortuna.cz>
2020-06-21 21:18:24 +0200

    Fix lint

--- src/components/Header/test.spec.js
+++ src/components/Header/test.spec.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Provider} from 'react-redux';
+import { Provider } from 'react-redux';
 import { render } from '@testing-library/react';
 import configureMockStore from 'redux-mock-store';
 
